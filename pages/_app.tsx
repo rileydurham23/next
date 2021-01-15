f211dda7edd408cb856c794de959ea81426de599
Alexey Ivanov <mail@alexeyivanov.info>
2021-01-16 00:33:32 +0900

    Add analytics (#32)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,36 @@
+import { useEffect } from "react";
 import { ThemeProvider } from "styled-components";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import theme from "components/theme";
+import * as Fathom from "fathom-client";
 import "components/global-styles.css";
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <ThemeProvider theme={theme}>
-    <Component {...pageProps} />
-  </ThemeProvider>
-);
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (process.env.NODE_ENV !== "production") return;
+
+    Fathom.load(process.env.NEXT_PUBLIC_FATHOM_ID, {
+      includedDomains: [process.env.NEXT_PUBLIC_HOST],
+    });
+
+    const onRouteChangeComplete = () => Fathom.trackPageview();
+
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    const cleanup = () =>
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+
+    return cleanup;
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+};
 
 export default MyApp;
