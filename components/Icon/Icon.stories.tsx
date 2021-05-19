7f81f250b7d5614b5370880fef5f877f33a536f8
Artem Shibakov <shibakow@gmail.com>
2021-05-19 18:08:09 +0300

    Add all icons story (#250)

--- components/Icon/Icon.stories.tsx
+++ components/Icon/Icon.stories.tsx
@@ -1,6 +1,11 @@
 import { Story } from "@storybook/react";
 import Icon, { IconProps } from "./Icon";
+import { IconName } from "./types";
+import * as icons from "./icons";
 import Flex from "../Flex";
+import Box from "../Box";
+import { useState } from "react";
+import { transition } from "components/system";
 
 const Component: Story<IconProps> = (args) => (
   <Flex
@@ -48,3 +53,55 @@ ExtraLarge.args = {
   name: "bolt",
   size: "xl",
 };
+
+const AllIconsComponent: Story = () => {
+  const [search, setSearch] = useState("");
+  const iconsList = Object.keys(icons) as IconName[];
+  return (
+    <Flex
+      bg="white"
+      p="9"
+      width="100%"
+      height="100vh"
+      alignItems="center"
+      flexDirection="column"
+    >
+      <Box
+        as="input"
+        type="text"
+        value={search}
+        placeholder="finder"
+        p="2"
+        onChange={(event) => setSearch(event.target.value.toLowerCase())}
+      />
+      <Flex
+        color="purple"
+        width="100%"
+        height="100%"
+        flexWrap="wrap"
+        alignItems="center"
+        justifyContent="center"
+      >
+        {iconsList.map((icon) => (
+          <Flex
+            key={icon}
+            p="4"
+            flexDirection="column"
+            alignItems="center"
+            opacity={icon.toLowerCase().match(search) ? 1 : 0.15}
+            transition={transition([["opacity", "interaction"]])}
+          >
+            <Icon name={icon} />
+            <Box as="span" color="darkest" mt="2">
+              {icon}
+            </Box>
+          </Flex>
+        ))}
+      </Flex>
+    </Flex>
+  );
+};
+
+export const AllIcons = AllIconsComponent.bind({});
+
+AllIcons.args = {};
