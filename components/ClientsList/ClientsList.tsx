151a61e4e818bfb965b298ef95c04be6ffbdc073
Artem Shibakov <shibakow@gmail.com>
2021-05-27 16:34:00 +0300

    Move wrappers around clients list to component (#281)

--- components/ClientsList/ClientsList.tsx
+++ components/ClientsList/ClientsList.tsx
@@ -1,42 +1,41 @@
 import styled from "styled-components";
 import css from "@styled-system/css";
-import Flex, { FlexProps } from "components/Flex";
-import Box from "components/Box";
+import Flex from "components/Flex";
+import Box, { BoxProps } from "components/Box";
+import { Centrator } from "components/Layout";
 import theme from "components/theme";
 import Company, { CompanyId } from "components/Company";
 import { StyledSystemProps } from "components/system";
 
-type Props = { clients: CompanyId[] } & FlexProps;
+type Props = { clients: CompanyId[] } & BoxProps;
 
 export default function ClientsList({ clients, ...props }: Props) {
   return (
-    <Flex
-      flexDirection="column"
-      alignItems="center"
-      width="100%"
-      py="6"
-      {...props}
-    >
-      <Box as="h2" fontSize="0" color="white">
-        Clients
-      </Box>
-      <Box as="p" fontSize="text-lg" lineHeight="lg" fontWeight="bold">
-        Trusted by Leading Organizations
-      </Box>
+    <Box as="section" {...props}>
+      <Centrator>
+        <Flex flexDirection="column" alignItems="center" width="100%" py="6">
+          <Box as="h2" fontSize="0" color="white">
+            Clients
+          </Box>
+          <Box as="p" fontSize="text-lg" lineHeight="lg" fontWeight="bold">
+            Trusted by Leading Organizations
+          </Box>
 
-      <StyledList>
-        {clients.map((id) => (
-          <StyledItem key={id}>
-            <Company
-              id={id}
-              px="4"
-              py={[3, 0]}
-              boxShadow="0 1px 3px rgba(0, 0, 0, 0.24);"
-            />
-          </StyledItem>
-        ))}
-      </StyledList>
-    </Flex>
+          <StyledList>
+            {clients.map((id) => (
+              <StyledItem key={id}>
+                <Company
+                  id={id}
+                  px="4"
+                  py={[3, 0]}
+                  boxShadow="0 1px 3px rgba(0, 0, 0, 0.24);"
+                />
+              </StyledItem>
+            ))}
+          </StyledList>
+        </Flex>
+      </Centrator>{" "}
+    </Box>
   );
 }
 
