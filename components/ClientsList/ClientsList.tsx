2b1bf055fe4fcbd39facc362e7a96fcc66960a79
Artem Shibakov <shibakow@gmail.com>
2021-05-28 14:12:11 +0300

    Clients list mobile styles (#284)

--- components/ClientsList/ClientsList.tsx
+++ components/ClientsList/ClientsList.tsx
@@ -3,38 +3,33 @@ import css from "@styled-system/css";
 import Flex from "components/Flex";
 import Box, { BoxProps } from "components/Box";
 import { Centrator } from "components/Layout";
-import theme from "components/theme";
 import Company, { CompanyId } from "components/Company";
 import { StyledSystemProps } from "components/system";
 
-type Props = { clients: CompanyId[] } & BoxProps;
+type Props = {
+  clients: CompanyId[];
+} & BoxProps;
 
 export default function ClientsList({ clients, ...props }: Props) {
   return (
-    <Box as="section" {...props}>
+    <Box as="section" py={[3, 6]} {...props}>
       <Centrator>
-        <Flex flexDirection="column" alignItems="center" width="100%" py="6">
+        <Flex flexDirection="column" alignItems="center" width="100%">
           <Box as="h2" fontSize="0" color="white">
             Clients
           </Box>
           <Box as="p" fontSize="text-lg" lineHeight="lg" fontWeight="bold">
             Trusted by Leading Organizations
           </Box>
-
           <StyledList>
             {clients.map((id) => (
               <StyledItem key={id}>
-                <Company
-                  id={id}
-                  px="4"
-                  py={[3, 0]}
-                  boxShadow="0 1px 3px rgba(0, 0, 0, 0.24);"
-                />
+                <Company id={id} px="4" py={[3, 0]} />
               </StyledItem>
             ))}
           </StyledList>
         </Flex>
-      </Centrator>{" "}
+      </Centrator>
     </Box>
   );
 }
@@ -44,8 +39,10 @@ const StyledItem = styled("li")<StyledSystemProps>(
     boxSizing: "border-box",
     height: ["auto", "100px"],
     my: 3,
-    width: ["100%", `calc(20% - ${theme.space[4]}px)`],
     ml: [0, 4],
+    width: ["50%", `calc(20% - 20px)`],
+    borderRadius: "default",
+    boxShadow: ["none", "0 1px 3px rgba(0, 0, 0, 0.24)"],
     "&:nth-child(5n+1)": {
       ml: [0, 0],
     },
@@ -56,11 +53,12 @@ const StyledList = styled("ul")<StyledSystemProps>(
   css({
     boxSizing: "border-box",
     display: "flex",
+    justifyContent: "space-between",
     flexWrap: "wrap",
     width: "100%",
     listStyle: "none",
     m: 0,
     p: 0,
-    mt: 6,
+    mt: [2, 6],
   })
 );
