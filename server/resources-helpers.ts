8e51efbfec21fd11e789fa93664f49f909b16972
Ekaterina Shibakova <41822696+Alqanar@users.noreply.github.com>
2021-11-19 16:42:59 +0300

    Added the white-papers index page (#601)
    * added white-papers index page

--- server/resources-helpers.ts
+++ server/resources-helpers.ts
@@ -2,20 +2,29 @@ import { PodcastEpisode } from "components/EpisodesList/types";
 import { getPagesInfo } from "./pages-helpers";
 
 export const getResourcesData = (type: string) => {
-  let podcastsPageInfo = [];
+  let resourcesPageInfo = [];
 
   try {
-    podcastsPageInfo = getPagesInfo(`resources/${type}/**/*.mdx`)
-      .map(({ data }) => data)
-      .filter((episode: PodcastEpisode) => episode.frontmatter.podcastName)
-      .sort((a: PodcastEpisode, b: PodcastEpisode) => {
-        const aNumberEpisode = Number(a.frontmatter.podcastName.split("-")[0]);
-        const bNumberEpisode = Number(b.frontmatter.podcastName.split("-")[0]);
-        return bNumberEpisode - aNumberEpisode;
-      });
+    resourcesPageInfo = getPagesInfo(`resources/${type}/**/*.mdx`).map(
+      ({ data }) => data
+    );
+
+    if (type === "podcast") {
+      resourcesPageInfo = resourcesPageInfo
+        .filter((episode: PodcastEpisode) => episode.frontmatter.podcastName)
+        .sort((a: PodcastEpisode, b: PodcastEpisode) => {
+          const aNumberEpisode = Number(
+            a.frontmatter.podcastName.split("-")[0]
+          );
+          const bNumberEpisode = Number(
+            b.frontmatter.podcastName.split("-")[0]
+          );
+          return bNumberEpisode - aNumberEpisode;
+        });
+    }
   } catch (e) {
     console.error(e);
   }
 
-  return podcastsPageInfo;
+  return resourcesPageInfo;
 };
