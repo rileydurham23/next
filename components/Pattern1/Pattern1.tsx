94cb8f3b5e462810e542d04e9d445482432570fc
Delia Constantino <delia.constantino@goteleport.com>
2021-11-30 11:37:30 -0500

    fix mobile and full screen background sizing

--- components/Pattern1/Pattern1.tsx
+++ components/Pattern1/Pattern1.tsx
@@ -35,6 +35,8 @@ export const Pattern1 = ({
         justifyContent="space-between"
         alignItems="center"
         // mobile height will need to be changed if longer forms are used
+        height={["100%", null]}
+        minHeight="100vh"
         pl={[2, 4]}
         pr={[2, 4]}
         {...background}
@@ -73,7 +75,7 @@ export const Pattern1 = ({
 
 const StyledCard = styled(Flex)(
   css({
-    maxWidth: ["343px", "944px"],
+    maxWidth: "944px",
     width: "100%",
     backgroundColor: "white",
     flexDirection: ["column", "row"],
