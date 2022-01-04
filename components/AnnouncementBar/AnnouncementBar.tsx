4bbf1dda123a2c425b287ba97f804a2beaae953b
Alexey Ivanov <mail@alexeyivanov.info>
2022-01-04 21:21:09 +0900

    Refactor AnnouncementBar (#704)

--- components/AnnouncementBar/AnnouncementBar.tsx
+++ components/AnnouncementBar/AnnouncementBar.tsx
@@ -1,231 +1,217 @@
-import styled, { keyframes, css as styledCss } from "styled-components";
+import { useState, useEffect } from "react";
+import styled from "styled-components";
 import css from "@styled-system/css";
-
-import Box from "components/Box";
-import Flex from "components/Flex";
 import Link from "components/Link";
-import { LogoRow, CaseStudyRow, Gray1, Gray2, Gray3 } from "./data";
-import * as logos from "./logos";
+import { LogoRow, Logo, Gray } from "./data";
+import { transition, variant } from "components/system";
 
-/**
- * The current version of this component works with implementations of 2 or 3 rows
- * Adding rows will require additional keyframes (ask Cole for help if you need it)
- */
+type AnnouncementBarType = "case" | "big" | "default";
 
-export type LogoName = keyof typeof logos;
-
-export interface AnnouncementBarLogoProps {
-  logo: LogoName;
-  inLivingColor?: boolean;
-  trim?: string | string[];
-}
-
-export interface AnnouncementBarProps {
-  inLivingColor?: boolean;
-  rows?: Array<LogoRow> | Array<CaseStudyRow>;
-  caseStudy?: boolean;
-}
-
-export interface CaseStudyLogoProps {
-  logo: LogoName;
+export interface ItemProps {
+  src: string;
   url?: string;
-  trim?: string | string[];
+  variant: AnnouncementBarType;
 }
 
-const AnnouncementBarLogo = ({
-  logo,
-  inLivingColor,
-  trim,
-}: AnnouncementBarLogoProps) => {
-  const LogoSVG = logos[logo];
-  return (
-    <Box
-      maxWidth={[120, 160]}
-      minWidth={[null, 80]}
-      height={inLivingColor ? 70 : 32}
-      my={2}
-      color="gray"
-      opacity={inLivingColor ? "1" : ".56"}
-      display={trim}
-    >
-      <LogoSVG width="100%" height="100%" display="block"></LogoSVG>
-    </Box>
-  );
-};
+export const Item = ({ src, url, variant }: ItemProps) => {
+  if (variant === "case") {
+    const boxProps = url
+      ? {
+          as: Link,
+          href: url,
+        }
+      : {};
 
-export const CaseStudyLogo = ({ logo, url, trim }: CaseStudyLogoProps) => {
-  const LogoSVG = logos[logo];
+    return (
+      <StyledCase link={!!url} {...boxProps}>
+        <StyledImage src={src} alt="" variant={variant} />
+      </StyledCase>
+    );
+  }
 
-  return url ? (
-    <StyledWrapper
-      display={trim}
-      border="1px solid #651fff"
-      css={css({
-        "&:hover": {
-          transition: "all .3s",
-          boxShadow: "0 4px 16px rgba(0, 0, 0, .24)",
-        },
-      })}
-    >
-      <Link href={url}>
-        <StyledBox>
-          <LogoSVG width="100%" height="80%"></LogoSVG>
-        </StyledBox>
-      </Link>
-    </StyledWrapper>
-  ) : (
-    <StyledWrapper display={trim}>
-      <StyledBox>
-        <LogoSVG width="100%" height="80%"></LogoSVG>
-      </StyledBox>
-    </StyledWrapper>
-  );
+  return <StyledImage src={src} alt="" variant={variant} />;
 };
+export interface AnnouncementBarProps {
+  type?: AnnouncementBarType;
+  rows?: LogoRow[];
+}
 
 export const AnnouncementBar = ({
-  inLivingColor = false,
-  rows = [Gray1, Gray2, Gray3],
-  caseStudy = false,
+  type = "default",
+  rows = Gray,
 }: AnnouncementBarProps) => {
+  const [currentRow, setCurrentRow] = useState(0);
+  const [animationPaused, setAnimationPaused] = useState(false);
+
+  useEffect(() => {
+    let current = currentRow;
+
+    const interval = setInterval(() => {
+      if (animationPaused) return;
+
+      const next = current < rows.length - 1 ? current + 1 : 0;
+
+      setCurrentRow(next);
+      current = next;
+    }, 5000);
+
+    return () => {
+      console.log("cleanup");
+      clearInterval(interval);
+    };
+  }, [rows.length, currentRow, animationPaused]);
+
   return (
     <>
-      {caseStudy && (
-        <StyledH3>
+      {type === "case" && (
+        <StyledHeader>
           Trusted by{" "}
           <Link href="/case-study/" scheme="site">
-            {" "}
             leading organizations
           </Link>
-        </StyledH3>
+        </StyledHeader>
       )}
-      <Flex
-        flexDirection="row"
-        justifyContent="center"
-        alignItems="center"
-        height="60px"
-        py={[6, 8]}
+      <StyledRowWrapper
+        variant={type}
+        onMouseEnter={() => setAnimationPaused(true)}
+        onMouseLeave={() => setAnimationPaused(false)}
       >
-        {rows.map((e, i) => {
+        {rows.map((row: LogoRow, index: number) => {
           return (
             <StyledRow
-              key={`row${i}`}
-              data-count={rows.length}
-              opacity={i === 0 ? "1" : "0"}
-              animationDelay={`${i * 5}s`}
-              animationDuration={`${rows.length * 5}s`}
-              visibility={i === 0 ? "visible" : "hidden"}
+              key={`row${index}`}
+              hidden={currentRow !== index}
+              variant={type}
             >
-              {e.map((company, index: number) => {
-                return caseStudy ? (
-                  <CaseStudyLogo
-                    key={index}
-                    logo={company["imageName"]}
-                    url={company.url}
-                    trim={index > 2 ? ["none", "flex"] : "flex"}
-                  />
-                ) : (
-                  <AnnouncementBarLogo
-                    key={company}
-                    inLivingColor={inLivingColor}
-                    logo={company as LogoName}
-                    trim={index > 2 ? ["none", "flex"] : "flex"}
-                  />
-                );
-              })}
+              {row.map((company: Logo) => (
+                <Item
+                  key={company.src}
+                  src={company.src}
+                  url={company.url}
+                  variant={type}
+                />
+              ))}
             </StyledRow>
           );
         })}
-      </Flex>
+      </StyledRowWrapper>
     </>
   );
 };
 
-const twoRowAnimation = keyframes`
-        0% {
-          opacity: 0;
-          visibility: visible;
-        }
-        50% {
-          opacity: 1;
-        }
-        51% {
-          opacity: 0;
-          visibility: hidden;
-        }
-        100% {
-          opacity: 0;
-          visibility: hidden;
-        }
-        `;
-
-const threeRowAnimation = keyframes`
-        0% {
-          opacity: 0;
-          visibility: visible;
-        }
-        33% {
-          opacity: 1;
-        }
-        34% {
-          opacity: 0;
-          visibility: hidden;
-        }
-        100% {
-          opacity: 0;
-          visibility: hidden;
-        }
-        `;
-
-const StyledBox = styled(Flex)(
+const StyledHeader = styled("h3")(
   css({
-    p: 2,
-    maxWidth: ["100%", 160],
-    height: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: "16px",
-  })
-);
-
-const StyledWrapper = styled(Flex)(
-  css({
-    width: 206,
-    height: 120,
-    flexShrink: 1,
-    backgroundColor: "white",
-    borderRadius: "16px",
-    boxShadow: "0 1px 4px rgb(0 0 0 / 24%)",
-    boxSizing: "border-box",
-    justifyContent: "center",
-    alignItems: "center",
-  })
-);
-
-const StyledRow = styled(Flex)((props) => [
-  css({
-    px: 3,
-    minHeight: ["48px", "60px"],
-    width: "100%",
-    flexDirection: "row",
-    flexWrap: "nowrap",
-    gap: "30px",
-    justifyContent: "center",
-    alignItems: "center",
-    position: "absolute",
-    animationIterationCount: "infinite",
-  }),
-  styledCss`animation-name: ${
-    props["data-count"] === 2 ? twoRowAnimation : threeRowAnimation
-  };`,
-]);
-
-const StyledH3 = styled("h3")(
-  css({
+    marginBottom: [2, 3],
     color: "dark-purple",
     fontSize: ["text-lg", "text-xl"],
-    textAlign: "center",
-    marginBottom: [5, 3],
     lineHeight: "xxl",
     fontWeight: "bold",
+    textAlign: "center",
+  })
+);
+
+const StyledRowWrapper = styled("div")<{ variant: AnnouncementBarType }>(
+  css({
+    position: "relative",
+  }),
+  variant({
+    variants: {
+      default: {
+        height: "112px",
+      },
+      big: {
+        height: "112px",
+      },
+      case: {
+        height: ["60px", "120px"],
+      },
+    },
+  })
+);
+
+const StyledRow = styled("div")<{
+  hidden: boolean;
+  variant: AnnouncementBarType;
+}>(
+  ({ hidden }) =>
+    css({
+      position: "absolute",
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
+      zIndex: hidden ? 0 : 1,
+      display: "flex",
+      flexDirection: "row",
+      flexWrap: "nowrap",
+      gap: ["12px", "30px"],
+      justifyContent: "center",
+      alignItems: "center",
+      opacity: hidden ? "0" : "1",
+      transition: hidden ? "none" : "opacity 2.5s",
+      "& > *:nth-child(1n+4)": {
+        display: ["none", "flex"],
+      },
+    }),
+  variant({
+    variants: {
+      default: {
+        py: 6,
+      },
+      big: {
+        py: "21px",
+      },
+    },
+  })
+);
+
+const StyledCase = styled("div")<{
+  link: boolean;
+  variant: AnnouncementBarType;
+}>(({ link }) =>
+  css({
+    boxSizing: "border-box",
+    display: "flex",
+    flex: "0 1 206px",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
+    p: 2,
+    borderRadius: ["md", "lg"],
+    border: link ? "1px solid #651fff" : "none",
+    backgroundColor: "white",
+    boxShadow: "0 1px 4px rgb(0 0 0 / 24%)",
+    transition: transition([["boxShadow", "interaction"]]),
+    "&:hover": link
+      ? {
+          boxShadow: "0 4px 16px rgba(0, 0, 0, .24)",
+        }
+      : {},
+  })
+);
+
+const StyledImage = styled("img")<{ variant: AnnouncementBarType }>(
+  css({
+    display: "flex",
+    width: "auto",
+  }),
+  variant({
+    variants: {
+      default: {
+        maxWidth: [120, 160],
+        minWidth: [null, 80],
+        maxHeight: "100%",
+        opacity: "0.56",
+      },
+      big: {
+        maxWidth: [120, 160],
+        minWidth: [null, 80],
+        maxHeight: "100%",
+      },
+      case: {
+        maxWidth: ["100%", "144px"],
+        maxHeight: "80%",
+      },
+    },
   })
 );
