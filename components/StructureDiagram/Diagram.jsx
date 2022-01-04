f4ef3a0963e671f9d97c89f0c101b33f238bd1ed
C-STYR <73362670+C-STYR@users.noreply.github.com>
2022-01-04 17:45:38 +0000

    updates HowItWorks pages (#731)
    * updates HowItWorks pages
    
    * updated image

--- components/StructureDiagram/Diagram.jsx
+++ components/StructureDiagram/Diagram.jsx
@@ -14,6 +14,7 @@ import DiagramData from "./data";
 */
 import serverImage from "./images/server.svg";
 import databaseImage from "./images/database.svg";
+import desktopImage from "./images/desktop.svg";
 import auditImage from "./images/audit.svg";
 import usersImage from "./images/user.svg";
 import tunnelImage from "./images/tunnel.svg";
@@ -67,6 +68,7 @@ export class StructureDiagram extends React.Component {
     return infobox;
   }
 
+  // Green arrows/lines that appear in the default view pointing to Proxy
   renderProxyArrows() {
     let arrows = null;
     const { selected } = this.state;
@@ -84,6 +86,7 @@ export class StructureDiagram extends React.Component {
           <StyledProxyArrow8 color="green" direction="right" />
           <StyledProxyArrow9 color="green" direction="left" />
           <StyledProxyArrow10 color="green" direction="right" />
+          <StyledProxyArrow11 color="green" direction="left" />
           <StyledProxyLine1 color="green" direction="vertical" />
           <StyledProxyLine2 color="green" direction="vertical" />
           <StyledProxyLine3 color="green" direction="horizontal" />
@@ -97,6 +100,7 @@ export class StructureDiagram extends React.Component {
           <StyledProxyLine10 color="green" direction="vertical" />
           <StyledProxyLine11 color="green" direction="vertical" />
           <StyledProxyLine14 color="green" direction="horizontal" />
+          <StyledProxyLine13 color="green" direction="horizontal" />
           <StyledProxyLine12 color="green" direction="horizontal" />
         </>
       );
@@ -105,6 +109,7 @@ export class StructureDiagram extends React.Component {
     return arrows;
   }
 
+  // Red lines/arrows that point to Auth when Auth or Audit Log are clicked
   renderAuditArrows() {
     let arrows = null;
     const { selected } = this.state;
@@ -126,6 +131,9 @@ export class StructureDiagram extends React.Component {
           <StyledAuditLine8 color="red" direction="horizontal" />
           <StyledAuditLine9 color="red" direction="horizontal" />
           <StyledAuditLine11 color="red" direction="horizontal" />
+          <StyledAuditLine12 color="red" direction="horizontal" />
+          <StyledAuditLine13 color="red" direction="horizontal" />
+          <StyledAuditLine14 color="red" direction="horizontal" />
           <StyledAuditLine10 color="red" direction="vertical" />
         </>
       );
@@ -149,6 +157,7 @@ export class StructureDiagram extends React.Component {
     const isAuditSelected = this.state.kind === "audit" ? true : false;
     const isAuthSelected = this.state.kind === "authserver" ? true : false;
     const isUsersSelected = this.state.kind === "users" ? true : false;
+    const isDesktopSelected = this.state.kind === "desktop" ? true : false;
 
     return (
       <StyledContainer>
@@ -207,6 +216,15 @@ export class StructureDiagram extends React.Component {
             <img alt="database" src={databaseImage} />
             <h4>Database</h4>
           </StyledDatabaseIButton>
+          <StyledDesktopIButton
+            className="is-small"
+            selected={isDesktopSelected}
+            title="Desktop"
+            onClick={() => this.changeSelectedItem("desktop")}
+          >
+            <img alt="Desktop" src={desktopImage} />
+            <h4>Desktop Access</h4>
+          </StyledDesktopIButton>
         </StyledEdge>
 
         <StyledNetwork title="Private Network">
@@ -268,6 +286,14 @@ export class StructureDiagram extends React.Component {
             <img alt="webapps" src={webappImage} />
             <h4>Web App</h4>
           </StyledWepAppPButton>
+          <StyledDesktopPButton
+            selected={isDesktopSelected}
+            title="Desktop"
+            onClick={() => this.changeSelectedItem("desktop")}
+          >
+            <img alt="desktop" src={desktopImage} />
+            <h4>Desktop Access</h4>
+          </StyledDesktopPButton>
         </StyledNetwork>
 
         {this.renderInfoBox()}
@@ -277,29 +303,35 @@ export class StructureDiagram extends React.Component {
   }
 }
 
-// Proxy Lines
+// Proxy Lines & Arrows
 const StyledProxyArrow1 = styled(Arrow)`
   top: 230px;
   left: -9px;
 `;
+
+//arrow to Web App in PRIVATE NETWORK
 const StyledProxyArrow2 = styled(Arrow)`
-  bottom: 200px;
-  right: 128px;
+  bottom: 270px;
+  right: 126px;
 `;
+
+//arrow to Desktop in PRIVATE NETWORK
 const StyledProxyArrow3 = styled(Arrow)`
-  bottom: 95px;
-  right: 128px;
+  bottom: 86px;
+  right: 126px;
 `;
 const StyledProxyArrow4 = styled(Arrow)`
   top: 165px;
-  right: 128px;
+  right: 126px;
 `;
+//arrow to K8s in EDGE NETWORK
 const StyledProxyArrow5 = styled(Arrow)`
-  bottom: 204px;
+  bottom: 274px;
   left: -130px;
 `;
+//arrow to Web App in EDGE NETWORK
 const StyledProxyArrow6 = styled(Arrow)`
-  bottom: 124px;
+  bottom: 194px;
   left: -130px;
 `;
 const StyledProxyArrow7 = styled(Arrow)`
@@ -307,26 +339,40 @@ const StyledProxyArrow7 = styled(Arrow)`
   left: -130px;
 `;
 const StyledProxyArrow8 = styled(Arrow)`
-  bottom: 104px;
-  right: 170px;
+  bottom: 174px;
+  right: 126px;
 `;
+
+//Arrow for Database in EDGE NETWORK
 const StyledProxyArrow9 = styled(Arrow)`
-  bottom: 44px;
+  bottom: 114px;
   left: -130px;
 `;
+
+//Arrow for SSH NOde in PRIVATE NETWORK
 const StyledProxyArrow10 = styled(Arrow)`
   top: 78px;
-  right: 128px;
+  right: 126px;
 `;
+
+//Arrow for Desktop in EDGE NETWORK
+const StyledProxyArrow11 = styled(Arrow)`
+  bottom: 35px;
+  left: -130px;
+`;
+
+//Proxy Lines are GREEN
 const StyledProxyLine1 = styled(Line)`
   height: 72px;
   left: -2px;
   top: 303px;
 `;
+
+//long vertical GREEN line in PRIVATE NETWORK
 const StyledProxyLine2 = styled(Line)`
   right: 164px;
-  height: 300px;
-  bottom: 102px;
+  height: 380px;
+  bottom: 92px;
 `;
 const StyledProxyLine3 = styled(Line)`
   width: 28px;
@@ -343,19 +389,25 @@ const StyledProxyLine4 = styled(Line)`
   right: 138px;
   top: 277px;
 `;
+
+//horizontal line to Desktop in PRIVATE NETWORK
 const StyledProxyLine5 = styled(Line)`
   width: 28px;
   right: 138px;
-  bottom: 102px;
+  bottom: 92px;
 `;
+
+//long horizontal line to DB in PRIVATE NETWORK
 const StyledProxyLine6 = styled(Line)`
-  width: 200px;
+  width: 240px;
   left: -1px;
   top: 373px;
 `;
+
+//horizontal line to SSH Node in EDGE NETWORK
 const StyledProxyLine7 = styled(Line)`
   width: 40px;
-  left: -120px;
+  left: -116px;
   top: 193px;
 `;
 const StyledProxyLine8 = styled(Line)`
@@ -363,15 +415,19 @@ const StyledProxyLine8 = styled(Line)`
   left: -114px;
   top: 272px;
 `;
+
+//horizontal line to Web App in EDGE NETWORK
 const StyledProxyLine9 = styled(Line)`
   width: 40px;
-  left: -120px;
-  bottom: 130px;
+  left: -116px;
+  bottom: 200px;
 `;
+
+// long GREEN vertical line in EDGE NETWORK
 const StyledProxyLine10 = styled(Line)`
-  height: 234px;
-  left: -82px;
-  bottom: 55px;
+  height: 320px;
+  left: -76px;
+  bottom: 42px;
 `;
 const StyledProxyLine11 = styled(Line)`
   height: 186px;
@@ -383,37 +439,54 @@ const StyledProxyLine12 = styled(Line)`
   left: -146px;
   top: 49px;
 `;
-const StyledProxyLine14 = styled(Line)`
+
+//horizontal line to Desktop in EDGE NETWORK
+const StyledProxyLine13 = styled(Line)`
   width: 40px;
-  left: -120px;
-  bottom: 50px;
+  left: -116px;
+  top: 512px;
 `;
 
-// Audit Lines
+//horizontal line to Database in EDGE NETWORK
+const StyledProxyLine14 = styled(Line)`
+  width: 40px;
+  left: -116px;
+  bottom: 120px;
+`;
+
+// Audit Lines are RED
 
 const StyledAuditArrow1 = styled(Arrow)`
   top: 127px;
   left: 102px;
 `;
+
+//Arrow from nodes to Auth in PRIVATE NETWORK
 const StyledAuditArrow2 = styled(Arrow)`
-  bottom: 200px;
+  bottom: 270px;
   right: 222px;
 `;
+
+//Arrow from Proxy to Auth
 const StyledAuditArrow3 = styled(Arrow)`
-  bottom: 200px;
-  left: 68px;
+  bottom: 274px;
+  left: 70px;
 `;
+
+//Arrow from K8s to Proxy in EDGE NETWORK
 const StyledAuditArrow4 = styled(Arrow)`
-  bottom: 200px;
-  left: -46px;
+  bottom: 274px;
+  left: -40px;
 `;
 const StyledAuditLine1 = styled(Line)`
   height: 108px;
   left: 109px;
   top: 143px;
 `;
+
+//long vertical RED line in PRIVATE NETWORK
 const StyledAuditLine2 = styled(Line)`
-  height: 248px;
+  height: 398px;
   right: 164px;
   bottom: 80px;
 `;
@@ -432,35 +505,66 @@ const StyledAuditLine5 = styled(Line)`
   right: 140px;
   bottom: 80px;
 `;
+
+//horizontal line from Proxy to Auth
 const StyledAuditLine6 = styled(Line)`
   width: 40px;
   left: 32px;
-  top: 277px;
+  top: 273px;
 `;
+
+//horizontal line to SSH Node in EDGE NETWORK
 const StyledAuditLine7 = styled(Line)`
   width: 40px;
   left: -120px;
-  top: 198px;
+  top: 194px;
 `;
+
+//horizontal line to K8s in EDGE NETWORK
 const StyledAuditLine8 = styled(Line)`
   width: 80px;
   left: -120px;
-  top: 277px;
+  top: 273px;
 `;
+
+//horizontal line to Web App in EDGE NETWORK
 const StyledAuditLine9 = styled(Line)`
   width: 40px;
   left: -120px;
-  bottom: 128px;
+  bottom: 200px;
 `;
+
+//long RED vertical line in EDGE NETWORK
 const StyledAuditLine10 = styled(Line)`
-  height: 234px;
-  left: -82px;
-  bottom: 52px;
+  height: 320px;
+  left: -77px;
+  bottom: 42px;
 `;
+
+//horiztontal line to Database in EDGE NETWORK
 const StyledAuditLine11 = styled(Line)`
   width: 40px;
   left: -120px;
-  bottom: 50px;
+  bottom: 120px;
+`;
+
+//horiztontal line to Desktop in EDGE NETWORK
+const StyledAuditLine12 = styled(Line)`
+  width: 40px;
+  left: -120px;
+  bottom: 40px;
+`;
+//horiztontal line to Database in PRIVATE NETWORK
+const StyledAuditLine13 = styled(Line)`
+  width: 24px;
+  right: 140px;
+  bottom: 180px;
+`;
+//horiztontal line to SSH Node in PRIVATE NETWORK
+const StyledAuditLine14 = styled(Line)`
+  width: 24px;
+  right: 140px;
+  bottom: 476px;
 `;
 
 const StyledContainer = styled.div`
@@ -477,11 +581,12 @@ const StyledInternet = styled(NetworkBox)`
 const StyledEdge = styled(NetworkBox)`
   left: 0;
   top: 176px;
-  height: 344px;
+  height: 414px;
   width: 176px;
 `;
 const StyledNetwork = styled(NetworkBox)`
   left: 264px;
+  height: 558px;
   width: 380px;
 `;
 const StyledAuditButton = styled(ServiceButton)`
@@ -500,8 +605,8 @@ const SelectedTunnel = (props) => {
 const StyledTunnelImage = styled.img`
   position: absolute;
   cursor: pointer;
-  right: -73px;
-  bottom: 43px;
+  right: -96px;
+  bottom: 33px;
   margin: 0 !important;
   z-index: 1;
   ${SelectedTunnel}
@@ -520,25 +625,32 @@ const StyledUsersButton = styled(DiagramButton)`
 const StyledNodeIButton = styled(DiagramButton)`
   position: absolute;
   left: 40px;
-  bottom: 248px;
+  bottom: 323px;
   z-index: 1;
 `;
 const StyledDatabaseIButton = styled(DiagramButton)`
   position: absolute;
-  left: 40px;
-  bottom: 8px;
+  left: 38px;
+  bottom: 78px;
   z-index: 1;
 `;
 const StyledK8SIButton = styled(DiagramButton)`
   position: absolute;
   left: 40px;
-  bottom: 168px;
+  bottom: 242px;
   z-index: 1;
 `;
 const StyledWepAppIButton = styled(DiagramButton)`
   position: absolute;
   left: 40px;
-  bottom: 88px;
+  bottom: 158px;
+  z-index: 1;
+`;
+
+const StyledDesktopIButton = styled(DiagramButton)`
+  position: absolute;
+  left: 40px;
+  bottom: 0px;
   z-index: 1;
 `;
 
@@ -553,26 +665,36 @@ const StyledAuditLogButton = styled(DiagramButton)`
 const StyledK8SPButton = styled(DiagramButton)`
   position: absolute;
   right: 40px;
-  bottom: 244px;
+  bottom: 314px;
   z-index: 1;
 `;
 const StyledNodePButton = styled(DiagramButton)`
   position: absolute;
   right: 40px;
-  bottom: 348px;
+  bottom: 418px;
   z-index: 1;
 `;
 const StyledWepAppPButton = styled(DiagramButton)`
   position: absolute;
   right: 40px;
-  bottom: 136px;
+  bottom: 216px;
   z-index: 1;
 `;
 const StyledDatabasePButton = styled(DiagramButton)`
   position: absolute;
   right: 40px;
-  bottom: 36px;
+  bottom: 116px;
   z-index: 1;
+`;
+
+const StyledDesktopPButton = styled(DiagramButton)`
+  position: absolute;
+  right: 40px;
+  bottom: 20px;
+  z-index: 1;
+  h4 {
+    white-space: nowrap;
+  }
 `;
 
 export default StructureDiagram;
