17b952a64efec8ab4b77cc0fc76d4d9a9b0636d5
Sandy Cruz <sandyleahcruz@icloud.com>
2022-01-10 14:36:35 -0800

    Make initial skeleton

---
+++ components/Download/Download.tsx
@@ -0,0 +1,13 @@
+import Centrator from "components/Centrator";
+import Section from "components/Section";
+
+let getData;
+
+export const Download = () => {
+  return (
+    <Centrator>
+      <h1>hiiii</h1>
+      <h1>bye</h1>
+    </Centrator>
+  );
+};
