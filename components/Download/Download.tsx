1e5f34772922fc9164565b2b41dd4fcc961007e5
Sandy Cruz <sandyleahcruz@icloud.com>
2022-01-11 11:22:30 -0800

    Start migrating /downloads

--- components/Download/Download.tsx
+++ components/Download/Download.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import css from "@styled-system/css";
+import styled from "styled-components";
 
+import * as _ from "lodash";
+import Box from "components/Box";
 import Centrator from "components/Centrator";
-import Section from "components/Section";
+import GitHubButton from "react-github-btn";
+// import Section from "components/Section";
 
-const urls = {
-  teleport:
-    "https://dashboard.gravitational.com/webapi/releases-oss?product=teleport&page=0",
-  gravity:
-    "https://dashboard.gravitational.com/webapi/releases-oss?product=gravity&page=0",
-};
+const gravity =
+  "https://dashboard.gravitational.com/webapi/releases-oss?product=gravity&page=0";
+const teleport =
+  "https://dashboard.gravitational.com/webapi/releases-oss?product=teleport&page=0";
 
 const fetchDownloads = (): Promise<unknown> => {
   const url =
@@ -24,38 +27,104 @@ interface ReturnObject {
 
 const getServerSideProps = (context): Promise<ReturnObject> => {
   return fetchDownloads().then((response) => {
-    console.log(response);
+    // console.log(response);
     return {
       props: {},
     };
   });
 };
 
-export const Download = (product) => {
+const groupByMajorVersions = (allReleases, product) => {
+  const versions = {};
+
+  allReleases.forEach((release) => {
+    const majorVersion =
+      product === "teleport"
+        ? release.version.slice(1, 4)
+        : release.version.slice(0, 3);
+
+    if (versions[majorVersion]) {
+      versions[majorVersion].push(release);
+    } else {
+      versions[majorVersion] = [release];
+    }
+  });
+
+  const sortedVersions = _(versions).toPairs().sortBy(0).fromPairs().value();
+  const versionsArray = _.values(sortedVersions).reverse();
+
+  return versionsArray;
+};
+
+export const Download = (product: "teleport" | "gravity") => {
   const [downloads, setDownloads] = useState([]);
   const [error, setError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const [showNotes, setShowNotes] = useState(false);
-  // const url = product === "teleport" ? "teleport" : "gravity";
+  const url = product === "teleport" ? teleport : gravity;
+
+  const renderGithubStars = () => {
+    const url =
+      product === "teleport"
+        ? "https://github.com/gravitational/teleport"
+        : "https://github.com/gravitational/gravity";
+
+    return (
+      <GitHubButton
+        href={url}
+        data-icon="octicon-star"
+        data-size="large"
+        data-show-count="true"
+        aria-label="Star repo on GitHub"
+      >
+        Star
+      </GitHubButton>
+    );
+  };
+
+  const renderNotesToggle = () => {
+    const label = showNotes
+      ? "Hide All Release Notes"
+      : "Show All Release Notes";
+    return (
+      <StyledNotesButton onClick={toggleReleaseNotes}>
+        {label}
+      </StyledNotesButton>
+    );
+  };
 
   const toggleReleaseNotes = () => {
     setShowNotes(!showNotes);
   };
 
   useEffect(() => {
-    const url =
-      "https://dashboard.gravitational.com/webapi/releases-oss?product=teleport&page=0";
-
     fetch(url)
       .then((response) => response.json())
-      .then((response) => {
-        console.log(response);
+      .then((data) => {
+        const majorVersions = groupByMajorVersions(data.items, data.product);
+        setDownloads(majorVersions);
+      })
+      .catch((error) => {
+        setError(false);
+        setErrorMessage(error);
       });
-  }, []);
+  }, [url]);
 
-  return (
-    <Centrator>
-      <h1>testing</h1>
-    </Centrator>
-  );
+  return <Centrator>adsfadfadf</Centrator>;
 };
+
+const StyledNotesButton = styled("button")(
+  css({
+    background: "white",
+    border: "1px solid grey",
+    borderRadius: "4px",
+    color: "blue",
+    fontSize: "12px",
+    height: "32px",
+    cursor: "pointer",
+    outline: "none",
+    margin: "0 32px 0 0",
+    display: "inline-block",
+    padding: "0 24px",
+  })
+);
