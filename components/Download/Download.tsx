298177150bb966fb36854300a5091a4e67f905a1
Sandy Cruz <sandyleahcruz@icloud.com>
2022-01-10 17:51:20 -0800

    Start migrating download page

--- components/Download/Download.tsx
+++ components/Download/Download.tsx
@@ -1,13 +1,61 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
 import Centrator from "components/Centrator";
 import Section from "components/Section";
 
-let getData;
+const urls = {
+  teleport:
+    "https://dashboard.gravitational.com/webapi/releases-oss?product=teleport&page=0",
+  gravity:
+    "https://dashboard.gravitational.com/webapi/releases-oss?product=gravity&page=0",
+};
 
-export const Download = () => {
+const fetchDownloads = (): Promise<unknown> => {
+  const url =
+    "https://dashboard.gravitational.com/webapi/releases-oss?product=teleport&page=0";
+
+  return fetch(url).then((response) => response.json());
+};
+
+interface ReturnObject {
+  props: any;
+}
+
+const getServerSideProps = (context): Promise<ReturnObject> => {
+  return fetchDownloads().then((response) => {
+    console.log(response);
+    return {
+      props: {},
+    };
+  });
+};
+
+export const Download = (product) => {
+  const [downloads, setDownloads] = useState([]);
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [showNotes, setShowNotes] = useState(false);
+  // const url = product === "teleport" ? "teleport" : "gravity";
+
+  const toggleReleaseNotes = () => {
+    setShowNotes(!showNotes);
+  };
+
+  useEffect(() => {
+    const url =
+      "https://dashboard.gravitational.com/webapi/releases-oss?product=teleport&page=0";
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+      });
+  }, []);
+
   return (
     <Centrator>
-      <h1>hiiii</h1>
-      <h1>bye</h1>
+      <h1>testing</h1>
     </Centrator>
   );
 };
