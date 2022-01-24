18a5b5d5329cd76dfedd2eb1d8e569dac8e0d6b0
Alexey Ivanov <mail@alexeyivanov.info>
2022-01-25 03:57:22 +0900

    Fix code blocks (#809)

--- layouts/HowItWorksPage/components.tsx
+++ layouts/HowItWorksPage/components.tsx
@@ -110,7 +110,7 @@ const StyledH5 = styled("h5")(
 );
 
 export const components = {
-  inlineCode: Code,
+  code: Code,
   img: Image,
   iframe: IFrame,
   h1: StyledH1,
@@ -123,7 +123,7 @@ export const components = {
   li: LI,
   table: StyledTable,
   video: Video,
-  code: BaseCode,
+  pre: BaseCode,
   a: function Link(props: LinkProps) {
     return <BaseLink {...props} scheme="site" />;
   },
