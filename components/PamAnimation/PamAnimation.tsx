52ec6c720a01291dbec2184b3195f7acb9152bc6
deliaconstantino <70108137+deliaconstantino@users.noreply.github.com>
2022-01-26 14:44:26 -0500

    `<WhyTeleport/>` Component for homepage (#773)
    * boilerplate
    
    * add image
    
    * update image
    
    * add images to assets
    
    * add white button svg
    
    * create base component
    
    * add images in target locations
    
    * experimentation
    
    * add commentLine to target position
    
    * add animation and timing for screen elements
    
    * update screen animations to use css only
    
    * add useWindowScroll hook to initiate animation
    
    * create AnimationSequence child component and begin animation condionally by location on viewport
    
    * update sample page for testing
    
    * clean code
    
    * add arm animation and update timing
    
    * update spacing
    
    * add 80% sizing for mobile and midway breakpoints
    
    * rename to WhyTeleport
    
    * update naming
    
    * spacing updates
    
    * update spacing
    
    * refactor code
    
    * update image sizes to 50% for mobile and 70% for mid breakpoint
    
    * use theme spacing
    
    * add WhyTeleport for homepage for testing
    
    * update spacing
    
    * add storybook entry
    
    * move animation into distinct component
    
    * update imports
    
    * add smaller title and image left options
    
    * update midpoint sizing to 80%
    
    * incorporate in existing components
    
    * update testing
    
    * remove unused code
    
    * update storybook
    
    * update animations
    
    * add scale to animations
    
    * update statisticScreen sizing
    
    * increase arm speed
    
    * move WhyTeleport to new homepage
    
    * refactor images to use intrinsic layout
    
    * update arm animation to fade and rise
    
    * fixed arm animation
    
    * update button for mobile
    
    * refactor SectionVisuals to take variants
    
    * update variant styles to match
    
    * clean variants
    
    * update imageLocation prop
    
    * remove unused exports
    
    * remove WhyTeleport and assemble on homepage
    
    * remove unused import
    
    Co-authored-by: C-STYR <cole@goteleport.com>

---
+++ components/PamAnimation/PamAnimation.tsx
@@ -0,0 +1,286 @@
+import NextImage from "next/image";
+import Box from "components/Box";
+import { useWindowScroll } from "react-use";
+
+import styled, { css as styledCss, keyframes } from "styled-components";
+import { css } from "components/system";
+import { fadeIn } from "../AnimationUtilities/AnimationUtilities";
+
+import whiteButton from "./assets/button-big.svg";
+import hand from "./assets/hand.svg";
+import shieldScreen from "./assets/shield-screen.svg";
+import codeScreen from "./assets/code-screen.svg";
+import passwordScreen from "./assets/password-screen.svg";
+import statisticsScreen from "./assets/statistics-screen.svg";
+import graphScreen from "./assets/graph-screen.svg";
+import commentLine from "./assets/commentline-screen.svg";
+import screenWhole from "./assets/screen-whole.svg";
+import PamNoHand from "./assets/pam-no-hand.svg";
+
+//data for six small screen animations
+const screens = [
+  {
+    animationDelay: "1500",
+    top: 34,
+    left: 35,
+    height: 65,
+    width: 100,
+    zIndex: "2",
+    src: shieldScreen,
+    alt: "shield screen",
+  },
+  {
+    animationDelay: "2300",
+    top: 95,
+    left: 15,
+    height: 98,
+    width: 100,
+    zIndex: "2",
+    src: codeScreen,
+    alt: "code screen",
+  },
+  {
+    animationDelay: "3200",
+    top: 50,
+    left: 482,
+    height: 128,
+    width: 135,
+    zIndex: "2",
+    src: statisticsScreen,
+    alt: "statistics screen",
+  },
+  {
+    animationDelay: "2800",
+    top: 46,
+    left: 539,
+    height: 98,
+    width: 100,
+    zIndex: "3",
+    src: passwordScreen,
+    alt: "password screen",
+  },
+  {
+    animationDelay: "3400",
+    top: 129,
+    left: 480,
+    height: 115,
+    width: 155,
+    zIndex: "3",
+    src: graphScreen,
+    alt: "graph screen",
+  },
+  {
+    animationDelay: "3900",
+    top: 121,
+    left: 477,
+    height: 63,
+    width: 27,
+    zIndex: "3",
+    src: commentLine,
+    alt: "comment line screen",
+  },
+];
+
+interface AnimatedImageContainerProps {
+  animationDelay: string;
+  top: number;
+  left: number;
+  height: number;
+  width: number;
+  src: string;
+  alt: string;
+  zIndex: string;
+}
+
+const AnimatedImageContainer = ({
+  animationDelay,
+  top,
+  left,
+  height,
+  width,
+  src,
+  alt,
+  zIndex,
+}: AnimatedImageContainerProps) => {
+  return (
+    <AnimatedImageWrapper
+      animationDelay={animationDelay}
+      top={[`${top * 0.5}px`, `${top * 0.8}px`, `${top}px`]}
+      left={[`${left * 0.5}px`, `${left * 0.8}px`, `${left}px`]}
+      width={[`${width * 0.5}px`, `${width * 0.8}px`, `${width}px`]}
+      height={[`${height * 0.5}px`, `${height * 0.8}px`, `${height}px`]}
+    >
+      <Box position="relative">
+        <NextImage
+          src={src}
+          alt={alt}
+          layout="intrinsic"
+          height={height}
+          width={width}
+          z-index={zIndex}
+        />
+      </Box>
+    </AnimatedImageWrapper>
+  );
+};
+
+const AnimationSequence = () => {
+  return (
+    <>
+      {/*White Button */}
+      <FadeInWrapper
+        animationDelay="1300"
+        top={["45px", "72px", "90px"]}
+        left={["119px", "190px", "238px"]}
+        height={["38px", "61px", "76px"]}
+        width={["150px", "240px", "300px"]}
+      >
+        <Box position="relative">
+          <NextImage
+            src={whiteButton}
+            alt="white button"
+            layout="intrinsic"
+            z-index="2"
+            height={76}
+            width={300}
+          />
+        </Box>
+      </FadeInWrapper>
+
+      {/*hand - final position*/}
+      <ArmWrapper
+        animationDelay="1100"
+        top={["62px", "99px", "124px"]}
+        left={["99px", "159px", "199px"]}
+        height={["49px", "78px", "98px"]}
+        width={["150px", "240px", "300px"]}
+      >
+        <NextImage
+          src={hand}
+          alt="Pam hand pointing"
+          layout="intrinsic"
+          z-index="3"
+          height={98}
+          width={300}
+        />
+      </ArmWrapper>
+
+      {/* six small screens */}
+      {screens.map((screen, idx) => (
+        <AnimatedImageContainer
+          animationDelay={screen.animationDelay}
+          top={screen.top}
+          left={screen.left}
+          height={screen.height}
+          width={screen.width}
+          zIndex={screen.zIndex}
+          src={screen.src}
+          alt={screen.alt}
+          key={idx}
+        />
+      ))}
+    </>
+  );
+};
+
+const PamAnimation = () => {
+  const { y } = useWindowScroll();
+
+  return (
+    <StyledImageBox mr={[0, 6, 4]} ml={[0, 0]} boxSizing="border-box">
+      {/* Pam no hand  */}
+      <Box
+        position="absolute"
+        top={["35px", "55px", "69px"]}
+        left={["55px", "87px", "109px"]}
+        height={["126px", "201px", "251px"]}
+        width={["150px", "240px", "300px"]}
+      >
+        <Box position="relative">
+          <NextImage
+            src={PamNoHand}
+            alt="Pam using single access log-in"
+            layout="intrinsic"
+            z-index="2"
+            height={251}
+            width={300}
+          />
+        </Box>
+      </Box>
+
+      {/* animation runs immediately if component is in viewport on page load */}
+      {y === 0 && <AnimationSequence />}
+      {/* animation runs when component scrolls into view */}
+      {!!y && <AnimationSequence />}
+    </StyledImageBox>
+  );
+};
+
+const imageRise = keyframes`
+  0% {
+    transform: translateY(20px);
+  }
+  100% {
+    transform: translateY(0px);
+  }
+`;
+
+const imageScale = keyframes`
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.3);
+  }
+  100% {
+    transform: scale(1);
+  }
+`;
+
+const armRaise = keyframes`
+  0% {
+    transform: rotate(65deg)
+  }
+  100% {
+    transform: rotate(0deg)
+  }
+`;
+
+const ArmWrapper = styled(Box)((props) => [
+  css({
+    position: "absolute",
+    transform: "rotate(65deg)",
+    transformOrigin: ["50px 38px", "70px 53px", "100px 75px"],
+  }),
+  styledCss`animation: 400ms linear ${props["animationDelay"]}ms 1 normal forwards running ${armRaise};`,
+]);
+
+const FadeInWrapper = styled(Box)((props) => [
+  css({
+    opacity: 0,
+    position: "absolute",
+  }),
+  styledCss`animation: 700ms linear ${props["animationDelay"]}ms 1 normal forwards running ${fadeIn};`,
+]);
+
+const AnimatedImageWrapper = styled(Box)((props) => [
+  css({
+    opacity: 0,
+    position: "absolute",
+    overflow: "hidden unscrollable",
+  }),
+  styledCss`animation: 800ms linear ${props["animationDelay"]}ms 1 normal forwards ${fadeIn}, 600ms linear ${props["animationDelay"]}ms ${imageRise}, 600ms linear ${props["animationDelay"]}ms ${imageScale};`,
+]);
+
+const StyledImageBox = styled(Box)(
+  css({
+    height: ["166px", "258px", "322px"],
+    width: ["332px", "531px", "664px"],
+    background: `url(${screenWhole})`,
+    backgroundSize: "cover",
+    position: "relative",
+    zIndex: "1",
+  })
+);
+
+export default PamAnimation;
--- components/SectionVisuals/SectionVisuals.tsx
+++ components/SectionVisuals/SectionVisuals.tsx
@@ -1,9 +1,12 @@
-import css from "@styled-system/css";
 import Centrator from "components/Centrator";
+import styled from "styled-components";
 import Box from "components/Box";
 import Flex from "components/Flex";
 import Heading from "components/Heading";
 import { P } from "components/MDX";
+import { variant } from "components/system";
+
+export type BoxStyleVariant = "default" | "secondary";
 
 export interface SectionVisualsProps {
   subtitle?: string;
@@ -11,6 +14,10 @@ export interface SectionVisualsProps {
   contentWidth?: string;
   children: React.ReactNode;
   description: React.ReactNode;
+  titleFontSize?: string | string[];
+  flexStyle?: string | string[];
+  imageLocation?: string;
+  boxStyle?: BoxStyleVariant;
 }
 
 export const SectionVisuals = ({
@@ -19,44 +26,31 @@ export const SectionVisuals = ({
   title,
   contentWidth = "600px",
   description,
+  titleFontSize = ["header-1", "48px"],
+  flexStyle = ["column", "row"],
+  imageLocation = "flex-start",
+  boxStyle = "default",
 }: SectionVisualsProps) => {
   return (
     <Centrator
       alignItems="stretch"
-      flexDirection={["column", "row"]}
       justifyContent="space-between"
+      flexDirection={flexStyle}
     >
       <Box flex="1 1 auto" maxWidth={contentWidth} pt={[4, 11]} pb={[0, 11]}>
         <Heading
           title={title}
           subtitle={subtitle}
-          titleFontSize={["header-1", "48px"]}
+          titleFontSize={titleFontSize}
           titleLineHeight={["xl", "xxl"]}
           titleFontWeight="black"
         />
-        <Box
-          mt={[3, 4]}
-          fontSize={["text-md", "text-xl"]}
-          lineHeight={["md", "lg"]}
-          color="dark-gray"
-          css={css({
-            [`& > ${P}`]: {
-              fontSize: "inherit",
-              lineHeight: "inherit",
-            },
-            li: {
-              color: "black",
-              fontSize: "text-md",
-              lineHeight: "md",
-            },
-          })}
-        >
-          {description}
-        </Box>
+        <StyledBox variant={boxStyle}>{description}</StyledBox>
       </Box>
       <Flex
         flex="0 0 auto"
         alignItems="center"
+        justifyContent={imageLocation}
         ml={[0, 5]}
         pt={[5, 8]}
         pb={[3, 8]}
@@ -67,3 +61,41 @@ export const SectionVisuals = ({
     </Centrator>
   );
 };
+
+const StyledBox = styled(Box)<{ variant: BoxStyleVariant }>(
+  variant({
+    variants: {
+      default: {
+        lineHeight: ["md", "lg"],
+        fontSize: ["text-md", "text-xl"],
+        mt: [3, 4],
+        color: "dark-gray",
+        [`& > ${P}`]: {
+          fontSize: "inherit",
+          lineHeight: "inherit",
+        },
+        li: {
+          color: "black",
+          fontSize: "text-md",
+          lineHeight: "md",
+        },
+      },
+      secondary: {
+        flexShrink: 2,
+        boxSizing: "border-box",
+        width: ["95vw", "100%", "480px"],
+        mx: "auto",
+        mt: [4, 4, 7],
+        p: {
+          fontSize: ["text-lg", "text-xl"],
+          fontFamily: "body",
+          fontWeight: "regular",
+          letterSpacing: "0.0015em",
+          lineHeight: "lg",
+          textAlign: "left",
+          color: "darkest",
+        },
+      },
+    },
+  })
+);
